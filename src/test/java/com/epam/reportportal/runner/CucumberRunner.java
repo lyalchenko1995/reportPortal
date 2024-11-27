3052d1a1ac9c4ab24f90dac074abeb64ea0da57b
Arsenii_Lialchenko <lyalchenko1995@gmail.com>
2024-11-27 08:33:09 +0200

    -added data provider method for CucumberRunner

--- src/test/java/com/epam/reportportal/runner/CucumberRunner.java
+++ src/test/java/com/epam/reportportal/runner/CucumberRunner.java
@@ -2,6 +2,7 @@ package com.epam.reportportal.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
 @CucumberOptions(
@@ -13,5 +14,11 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
   @Parameters({"featureFile"})
   public CucumberRunner(String featureFile) {
     System.setProperty("cucumber.features", featureFile);
+  }
+
+  @Override
+  @DataProvider(parallel = true)
+  public Object[][] scenarios() {
+    return super.scenarios();
   }
 }
